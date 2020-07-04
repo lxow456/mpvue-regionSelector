d4c9ed70022f6241bdef690dfd524a249ea859de
xiang li <lix423005@gmail.com>
2020-07-04 22:17:46 +0800

    update--初始化

---
+++ src/pages/index/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <div class="region">{{ province }} / {{ city }} / {{ district }}</div>
+    <button @click="handleClick" class="click-btn">click me</button>
+    <region-selector :isShow="selectorShow" province="山东省" city="济南市" district="历下区" @hideSelector="hideSelector"></region-selector>
+  </div>
+</template>
+
+<script>
+import regionSelector from '@/components/regionSelector';
+
+export default {
+  components: {
+    regionSelector
+  },
+  data() {
+    return {
+      selectorShow: false,
+      province: '山东省',
+      city: '济南市',
+      district: '历下区'
+    }
+  },
+  methods: {
+    handleClick() {
+      this.selectorShow = true;
+    },
+    hideSelector(info) {
+      console.log(info)
+      if (info) {
+        [this.province, this.city, this.district] = [info[0].name, info[1].name, info[2].name]
+      }
+      this.selectorShow = false;
+    }
+  }
+}
+
+</script>
+
+<style  scoped>
+.region {
+  display: flex;
+  justify-content: center;
+  height: 50px;
+  line-height: 50px;
+}
+.click-btn {
+  width: 200px;
+}
+
+</style>
